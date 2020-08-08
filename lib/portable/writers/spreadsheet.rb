270bb0d24d524c3c2b404d9e70e9b8ed5439a48a
Matthew Ruggio <mruggio@bluemarblepayroll.com>
2020-08-08 17:41:14 -0500

    initial implementation

--- lib/portable/writers/spreadsheet.rb
+++ lib/portable/writers/spreadsheet.rb
@@ -7,12 +7,60 @@
 # LICENSE file in the root directory of this source tree.
 #
 
+require 'fast_excel'
 require 'portable'
 
 module Portable
   module Writers
-    # Main writer class.
-    class Spreadsheet
+    # Can write documents to an Excel file.
+    class Spreadsheet < Base
+      def write!(filename:, data_provider: Data::Provider.new, time: Time.now.utc)
+        raise ArgumentError, 'filename is required' if filename.to_s.empty?
+
+        ensure_directory_exists(filename)
+
+        workbook = FastExcel.open(filename, constant_memory: true)
+
+        document.sheets.map do |sheet|
+          data_source = data_provider.data_source(sheet.name)
+
+          worksheet = workbook.add_worksheet(sheet.name)
+
+          write_head(worksheet, sheet, data_source)
+          write_data_table(worksheet, sheet, data_source, time)
+          write_foot(worksheet, sheet, data_source)
+        end
+
+        workbook.close
+
+        time_in_seconds = 1.0
+
+        [Result.new(filename, time_in_seconds)]
+      end
+
+      private
+
+      def write_head(worksheet, sheet, data_source)
+        sheet.header_rows.each { |row| worksheet.append_row(row) }
+
+        data_source.header_rows.each { |row| worksheet.append_row(row) }
+      end
+
+      def write_data_table(worksheet, sheet, data_source, time)
+        row_renderer = sheet_renderer.row_renderer(sheet.name, data_source.fields)
+
+        worksheet.append_row(row_renderer.headers) if sheet.include_headers?
+
+        data_source.data_rows.each do |row|
+          worksheet.append_row(row_renderer.render(row, time).values)
+        end
+      end
+
+      def write_foot(worksheet, sheet, data_source)
+        data_source.footer_rows.each { |row| worksheet.append_row(row) }
+
+        sheet.footer_rows.each { |row| worksheet.append_row(row) }
+      end
     end
   end
 end
